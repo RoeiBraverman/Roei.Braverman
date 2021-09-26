de24f1051714c68bed149146008af9c8f3ddf714
Roei Braverman <royebra@post.bgu.ac.il>
2021-09-26 22:27:33 +0300

    ex7

--- Roei_Braverman_app/app.js
+++ Roei_Braverman_app/app.js
@@ -5,11 +5,11 @@ const app = express();
 const path = require("path");
 
 
+
 // parse requests of content-type: application/json
-app.use(bodyParser.json());
+ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const publicDirectory = path.join(__dirname,'./public');
 app.use(express.static(publicDirectory));
@@ -17,25 +17,23 @@ app.use(express.static(publicDirectory));
 
 // simple route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"/views/CV.html"));
-  });
+res.sendFile(path.join(__dirname,"/views/CV.html"));
+});
 
 
-  // Create a route for getting all customers
+// Create a route for getting all customers
 app.get("/customers", function(req, res){
-  sql.query("SELECT * FROM customers", (err, mysqlres) => {
-  if (err) {
-  console.log("error: ", err);
-  res.status(400).send({message: "error in getting all customers: " + err}); return;
-  }
-              console.log("got all customers...");
-              res.send(mysqlres);
-              return;
-  }); });
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error in getting all customers: " + err}); return;
+    }
+                console.log("got all customers...");
+                res.send(mysqlres);
+                return;
+    }); });
 
 // set port, listen for requests 
 app.listen(8000, () => {
 console.log("Server is running on port 8000." );
 });
-
-
